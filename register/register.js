c50ac1ce2501c3ddd0f098d3765bb22455625395
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-04-12 07:31:43 -0700

    ok

--- register/register.js
+++ register/register.js
@@ -5,73 +5,80 @@ hamburger.addEventListener('click', ()=>{
     navbar.classList.toggle('active')
     hamburger.classList.toggle('active')
 })
-
 const form = document.querySelector('.form');
 const input = document.querySelectorAll('.input');
-
-
+ 
 const btnRegister= document.querySelector('.register');
 
-
-
-// form.addEventListener('click',function (e) {
-//     if (e.target.classList='input') {
-//         e.target.classList.toggle('acti')
-//     }
-// })
+console.log('HI');
 
 
 form.addEventListener('click', (event) => {
     if (event.target.classList.contains('input')) {
-      // Remove the acti class from all input fields
+
       const inputs = form.querySelectorAll('.input');
       inputs.forEach((input) => {
         input.classList.remove('acti');
       });
       
-      // Add the acti class to the clicked input field
+
       event.target.classList.add('acti');
     }
   });
-form.addEventListener('submit', function (event){
+  
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
-    let result = 0;
-    let result2 = 0;
+    let result1 = true;
+    let result2 = false;
+    let result3= false;
+  
+
     for (let index = 0; index < 5; index++) {
-        if (!input[index].value){
-           input[index].classList.add('danger')
-        }
-        else {
-            input[index].classList.remove('danger') 
-        }
-        let arr =[];
-        arr.push(input[index].value)
-        console.log( arr.every(checkAge));
-        function checkAge(age) {
-          return age;
-        }
-        result =arr.every(checkAge)
-        if (input[3].value===input[4].value) {
-            result2= true 
-            console.log('tru');
-        }
-        else {
-            result2= false 
-            console.log('xsc');
-        }
-       
+      if (!input[index].value) {
+        input[index].classList.add('danger');
+        result1 = false;
+      } else {
+        input[index].classList.remove('danger');
+      }
     }
-    console.log(result2);
-    if (result && result2) {
-        alert('You registered succesfully')
+  
+    let arr = [];
+    for (let index = 0; index < 5; index++) {
+      arr.push(input[index].value);
     }
-    else if (result) {
-        alert('password confirmation is incorrect')
-        input[3].classList.add('danger')
-        input[4].classList.add('danger')
+    if (arr.some((val) => val)) {
+    } else {
+      result1 = false;
     }
-    else {
-        alert('Make sure you enter all informations')
+  
+    if (input[3].value === input[4].value) {
+        result2  = true;
+    } else {
+      input[3].classList.add('danger');
+      input[4].classList.add('danger');
     }
-    });
-   
+  
+    if (input[3].value.length >= 8) {
+        result3 = true;
+    } else {
+      input[3].classList.add('danger');
+      input[4].classList.add('danger');
+    }
+
+    if (result1 && result2  && result3) {
+      alert('You registered successfully');
+    } else if (result1 && result2 ) {
+      alert('Password should have at least 8 characters');
+    } else if (result1) {
+      alert('Password confirmation is incorrect');
+      input[3].classList.add('danger');
+      input[4].classList.add('danger');
+    } else {
+      alert('Make sure you enter all information');
+    }
+  });
+
+
+
+
+
